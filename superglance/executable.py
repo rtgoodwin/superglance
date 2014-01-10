240ba1a6b0862c42be3c5d12cf7f0870023a4d69
Nate House <nathan.house@rackspace.com>
2014-01-10 11:09:27 -0600

    Added logging and config groups

--- superglance/executable.py
+++ superglance/executable.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 #
-# Copyright 2013 Richard Goodwin (some parts borrowed from Major Hayden's "supernova")
+# Copyright 2013 Richard Goodwin (some parts borrowed from Major Hayden's
+# "supernova"
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -15,9 +16,14 @@
 #   limitations under the License.
 #
 import getpass
+import logging
 import optparse
+import log_config
 import superglance
 import sys
+
+log_config.setup_logging()
+LOG = logging.getLogger(__name__)
 
 
 def gwrap(some_string):
@@ -39,7 +45,7 @@ def print_valid_envs(valid_envs):
     Prints the available environments.
     """
     print "[%s] Your valid environments are:" % (gwrap('Found environments'))
-    print "%r" % valid_envs
+    print "%r" % [x for x in valid_envs if x != 'log']
 
 
 def run_superglance():
@@ -47,24 +53,28 @@ def run_superglance():
     Handles all of the prep work and error checking for the
     superglance executable.
     """
+    envs = None
+
     s = superglance.SuperGlance()
     parser = optparse.OptionParser()
     parser.add_option('-d', '--debug', action="store_true",
-            dest="debug", default=False,
-            help='show glanceclient debug output')
+                            dest="debug", default=False,
+                            help='show glanceclient debug output')
     parser.add_option('-l', '--list', action="store_true",
-            dest="listenvs", default=False,
-            help='list all configured environments')
+                            dest="listenvs", default=False,
+                            help='list all configured environments')
 
     # Allow for passing --options all the way through to glanceclient
     parser.disable_interspersed_args()
     (opts, args) = parser.parse_args()
 
     # Is the config file missing or empty?
-    if s.get_glance_creds() == None:
-        print "[%s] Unable to find your superglance configuration file or " \
-              "your configuration file is malformed." % (
-                    rwrap("Configuration missing"))
+    if s.get_glance_creds() is None:
+        msg = "[%s] Unable to find your superglance configuration file or " \
+            "your configuration file is malformed." % (
+                rwrap("Configuration missing"))
+        print msg
+        LOG.error(msg)
         sys.exit()
 
     # Should we just list the available environments and exit?
@@ -78,28 +88,42 @@ def run_superglance():
 
     # Did we get a valid environment?
     try:
-        s.glance_env = args[0]
-        if not s.is_valid_environment():
-            print "[%s] Unable to find the %r environment in your " \
-                  "configuration file.\n" % (
-                    rwrap('Invalid environment'), args[0])
-            print_valid_envs(sorted(s.get_glance_creds().sections()))
-            sys.exit()
+        # Did we specify a config group?
+        if s.get_glance_creds().has_option(args[0], 'group'):
+            envs = format_envs(s.get_glance_creds().items(args[0]))
+        if not envs:
+            envs = [args[0]]
+        for env in envs:
+            s.glance_env = env
+            if not s.is_valid_environment():
+                msg = "[%s] Unable to find the %r environment in your " \
+                    "configuration file.\n" % (
+                        rwrap('Invalid environment'), s.glance_env)
+                print msg
+                LOG.error(msg)
+                print_valid_envs(sorted(s.get_glance_creds().sections()))
+                sys.exit()
     except IndexError:
-        print "[%s] A valid glance environment is required as the first " \
-              "argument.\n" % (rwrap("Environment missing"))
+        msg = "[%s] A valid glance environment is required as the first " \
+            "argument.\n" % (rwrap("Environment missing"))
+        print msg
+        LOG.error(msg)
         print_valid_envs(sorted(s.get_glance_creds().sections()))
         sys.exit()
 
     # Did we get any arguments to pass on to glance?
     if len(args) <= 1:
-        print "[%s] No arguments were provided to pass along to glance." % (
+        msg = "[%s] No arguments were provided to pass along to glance." % (
             rwrap('Missing glanceclient arguments'))
+        print msg
+        LOG.error(msg)
         sys.exit()
 
     # All of the remaining arguments should be handed off to glance
     glanceclient_args = args[1:]
-    s.run_glanceclient(glanceclient_args, opts.debug)
+    for env in envs:
+        s.glance_env = env
+        s.run_glanceclient(glanceclient_args, opts.debug)
 
 
 def run_superglance_keyring():
@@ -110,22 +134,26 @@ def run_superglance_keyring():
     s = superglance.SuperGlance()
     parser = optparse.OptionParser()
     parser.add_option('-g', '--get', action="store_true",
-            dest="get_password", default=False,
-            help='retrieves credentials from keychain storage')
+                      dest="get_password", default=False,
+                      help='retrieves credentials from keychain storage')
     parser.add_option('-s', '--set', action="store_true",
-            dest="set_password", default=False,
-            help='stores credentials in keychain storage')
+                      dest="set_password", default=False,
+                      help='stores credentials in keychain storage')
     (opts, args) = parser.parse_args()
 
     if opts.get_password and opts.set_password:
-        print "[%s] You asked to get and set a password at the same time. " \
+        msg = "[%s] You asked to get and set a password at the same time. " \
               "This is not supported." % rwrap("Too many options")
+        print msg
+        LOG.info(msg)
 
     # No matter what, we need two arguments: environment and a
     # configuration option
     if len(args) != 2:
-        print "[%s] Usage: superglance-keyring [--get | --set] " \
+        msg = "[%s] Usage: superglance-keyring [--get | --set] " \
               "environment parameter" % rwrap("Invalid number of arguments")
+        print msg
+        LOG.error(msg)
         sys.exit()
 
     username = "%s:%s" % (args[0], args[1])
@@ -147,8 +175,10 @@ def run_superglance_keyring():
 
         # Did we get a password from the prompt?
         if not password or len(password) < 1:
-            print "\n[%s] No data was altered in your keyring." % (
+            msg = "\n[%s] No data was altered in your keyring." % (
                 rwrap("Canceled"))
+            print msg
+            LOG.error(msg)
             sys.exit()
 
         # Try to store the password
@@ -158,11 +188,15 @@ def run_superglance_keyring():
             store_ok = False
 
         if store_ok:
-            print "\n[%s] Successfully stored credentials for %s under the " \
+            msg = "\n[%s] Successfully stored credentials for %s under the " \
                   "superglance service." % (gwrap("Success"), username)
+            print msg
+            LOG.info(msg)
         else:
-            print "\n[%s] Unable to store credentials for %s under the " \
+            msg = "\n[%s] Unable to store credentials for %s under the " \
                   "superglance service." % rwrap("Failed", username)
+            print msg
+            LOG.warning(msg)
 
         sys.exit()
 
@@ -187,7 +221,17 @@ def run_superglance_keyring():
             print "\n[%s] Found credentials for %s: %s" % (
                 gwrap("Success"), username, password)
         else:
-            print "\n[%s] Unable to retrieve credentials for %s.\nThere are " \
-                  "probably no credentials stored for this environment/" \
-                  "parameter combination (try --set)." % (
+            msg = "\n[%s] Unable to retrieve credentials for %s.\nThere are " \
+                "probably no credentials stored for this environment/" \
+                "parameter combination (try --set)." % (
                     rwrap("Failed"), username)
+            print msg
+            LOG.warning(msg)
+
+
+def format_envs(envs):
+    for param, value in envs:
+        if isinstance(value, basestring) and param.lower() == 'group':
+            for x in ['[', ']', '"', "'", ' ']:
+                value = value.replace(x, '')
+            return value.split(',')
